3a29d801ae1f2b3ebe2523e15f4880dae3c4252f
Ernesto Smith <113070060+erniesmith82@users.noreply.github.com>
2023-10-26 15:15:43 +0000

    hidden menu

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function openNav() {
     document.getElementById("mySidebar").style.width = "500px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("main").style.marginLeft = "210px";
   }
   
   function closeNav() {
