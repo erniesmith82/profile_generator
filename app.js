bbd43796a0d67efd94734a487839fc539d356673
Ernesto Smith <113070060+erniesmith82@users.noreply.github.com>
2023-10-27 03:36:01 +0000

    updated js

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+document.getElementById('submitBtn').addEventListener('click', function() {
+  let bgPic = document.getElementById('bgImage').value;
+  let picture = document.getElementById('profilePic').files[0];
+  let fstName = document.getElementById('firstName').value;
+  let lstName = document.getElementById('lastName').value;
+
+  
+  if (bgPic === 'Yes') {
+    let randomImage = "https://source.unsplash.com/random";
+    document.getElementById('backImage').src = randomImage;
+  } else if (bgPic === 'No') {
+    let noImageURL = "null"; 
+    document.getElementById('backImage').src = noImageURL;
+  
+  }
+
+  
+  if (picture) {
+    let reader = new FileReader();
+    reader.onload = function(event) {
+      document.getElementById('profilePic2').src = event.target.result;
+    };
+    reader.readAsDataURL(picture);
+  }
+
+  
+  document.getElementById('fname').textContent = fstName;
+  document.getElementById('lname').textContent = lstName;
+});
+
+
+
+
 function openNav() {
     document.getElementById("mySidebar").style.width = "500px";
     document.getElementById("main").style.marginLeft = "210px";
